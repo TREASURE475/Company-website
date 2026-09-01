247c02ed780eabfb85dc8a4eaafb2c889badb7b7
TREASURE475 <tenugeddy8@gmail.com>
2026-09-01 04:52:18 -0700

    Add home components

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import HeroSection from "@/components/home/herosection";
 import FeatureSection from "@/components/home/featuresection";
 import TestimonialSection from "@/components/home/testimonialsection";
-
 export default function Home() {
   return (
     <main>
---
+++ components/home/featuresection.tsx
@@ -0,0 +1,3 @@
+export default function FeatureSection() {
+  return <section>Feature Section</section>;
+}
